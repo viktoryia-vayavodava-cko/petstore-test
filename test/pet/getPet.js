2c99659e8754a18e152f7f5a2105af6d0155f26b
viktoryia-vayavodava-cko <viktoryia.vayavodava@checkout.com>
2020-03-16 12:03:34 +0000

    introduce another way of gettting the posted petId

--- test/pet/getPet.js
+++ test/pet/getPet.js
@@ -5,30 +5,27 @@ feature.only('Verify the get functionality', function () {
     scenario.only("Verify that correct pet is returned by id provided", function () {
 
         let context;
-        let id;
-        let pet;
+        let id = Math.floor(Math.random() * 1000000)
+        let pet = {
+            "id": `${id}`,
+            "category": {
+                "id": 130,
+                "name": "mice"
+            },
+            "name": "Leila",
+            "photoUrls": [
+                "http://t3est.com",
+                "http://t3est2.com"
+            ],
+            "status": "new"
+        }
 
-        given("we have a pet id ", async function () {
-            pet = {
-                "category": {
-                    "id": 130,
-                    "name": "mice"
-                },
-                "name": "Leila",
-                "photoUrls": [
-                    "http://t3est.com",
-                    "http://t3est2.com"
-                ],
-                "status": "new"
-            }
-
+        given("we create a pet with unique id ", async function () {
             context = await client.postPet(pet);
             id = context.body.id
-
         });
 
         when("we retrieve the pet by this id", async function () {
-            console.log(id);
             context = await client.getPet(id);
         });
 
@@ -37,9 +34,8 @@ feature.only('Verify the get functionality', function () {
         });
         and("the correct info is returned", function () {
             context.body.id.should.be.equal(id)
-            // context.body.status.should.be.equal('available')
-            context.body.name.should.be.equal('doggie')
-            context.body.category.name.should.be.equal('Dog')
+            context.body.name.should.be.equal('Leila')
+            context.body.category.name.should.be.equal('mice')
             // TO DO
             // change assertions accordingly and create new ones
         });
