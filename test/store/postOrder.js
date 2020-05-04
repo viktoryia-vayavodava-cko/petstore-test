3aae0e7ac4af7151a8a16f445936810a4ebeaaea
viktoryia-vayavodava-cko <viktoryia.vayavodava@checkout.com>
2020-05-04 13:52:23 +0100

    comments

--- test/store/postOrder.js
+++ test/store/postOrder.js
@@ -36,27 +36,32 @@ feature('User is able to place an order', function () {
         });
 
         and("status 200 is returned", function () {
-            context.status.should.be.equal(200); // assertion in Given step? ;) <-- @angye comment - moved under this "and" function
+            context.status.should.be.equal(200);
         });
 
         then("verify that the all fields are populated", function () {
             let response = context.body;
 
-            expect(response.id).to.not.be.undefined.and.not.be.null; // maybe its better to validate the type of value instead? what do you think ?
-            //  <-- @angye comment  yes - the assertion is is more pricese 
+            expect(response.id).to.not.be.undefined.and.not.be.null;
             expect(response.id, '"id" is not an integer').and.is.a('number').above(0).and.satisfy(Number.isInteger);
 
             expect(response.petId, 'petId is not undefined').to.not.be.undefined.and.not.be.null;
             expect(response.petId, 'petId is not a number').and.is.a('number').above(0).and.satisfy(Number.isInteger);
 
             //    response.quantity.should.be.equal(20); // for future reference - i would suggest using  1 notaion (either expect().to.be.. or should.be..)
-            // @angye reply - is this only for consistency or there is an acutal reaon to chose eiter? - when I asked on our last chat - you said I can use both
+            // @angye reply - is this only for consistency or there is an acutal reaon to chose eiter? - when I asked on our last chat - you said I can use both.
+
+            // @vicky --> ofcourse you can use both, but what i meant is that its better not to use both at the same time (in the same test suite), just pick 1 notation and stick to it :)   
+
             expect(response.quantity).to.be.equal(20);
             expect(response.shipDate).to.not.be.undefined.and.not.be.null; // its crucial to verify that ship date is populated correctly. please add a test for that
             // @angye reply - yes I have it - check below
+            // @vicky --> can you please point me to the line of code where you check that?
+
             expect(response.status, 'response is not set as "placed"').to.be.equal('placed');
             expect(response.status, '"status" is not a string').and.is.a('string');
 
+            // @vicky --> what is the reason for adding this part -'response is not set as "false"'- in the assertion?
             response.complete.should.be.equal(false, 'response is not set as "false"');
 
         });
@@ -71,6 +76,8 @@ feature('User is able to place an order', function () {
             (you can read here - https://github.com/chaijs/chai-things/issues/24) 
             for the sake of this test I undo the chai-thing library and related assertions.
             */
+            // @vicky --> yeah, i believe that chai-things wasn't added to initial project set up, right? you installed it afterwards? 
+            //was there a specific need for that?
             let response = context.body;
             //      expect(response).to.have.keys(["id", "petId", "quantity", "shipDate", "status", "complete"]).and.to.have.length(5);
             // this is a possible 
@@ -108,6 +115,7 @@ feature('User is able to place an order', function () {
             */
 
             // could you please also verify actual response received? e.g response.type, response.message?
+            // @vicky -> can you please address that comment as well? 
 
         });
         and("The return status is 500", function () {
@@ -122,7 +130,7 @@ feature('User is able to place an order', function () {
 
         let replacedOrder = {
             "id": order.id,
-            "petId": 111, // shouldn't the petId be integer? <-- @angye comment yes
+            "petId": 111,
             "quantity": 25,
             "shipDate": "2020-06-29T16:47:45.794",
             "status": "delivered",
@@ -130,12 +138,7 @@ feature('User is able to place an order', function () {
         };
 
         let context
-        // why did you create 2 contexts if you are using only 1 of them in your test? <-- @angye comment - 
-        /*  
-        because first I POST the order - which is order01
-        then I replace the same order with POST replacedOrder
-        I don't need to keep the previous value - but yes does not make sance to save the return body into a variable that I will soon replace - donce changed
-        */
+
         given("I have an existing order id", async function () {
             await client.postOrder(order);
         });
@@ -154,18 +157,6 @@ feature('User is able to place an order', function () {
             expect(replaceResponce.petId).to.be.equal(replacedOrder.petId);
             expect(replaceResponce.quantity).to.be.equal(replacedOrder.quantity);
 
-            /* you were having assertion error cuz actual date returned was 2020-06-29T16:37:45.794+0000. 
-            i think its better not to count how many items are in the date, but count the needless stuff from the end, e.g.
-            find the '+' sign and cut all starting from this '+' sign position =>
-            replaceResponce.shipDate.slice(0,replaceResponce.shipDate.indexOf('+')).should.be.equal(replacedOrder.shipDate)
-            but of course the slice itself should be coming from helper method
-
-            <-- @angye answer - well - after the fix I have made in  hempmethod.js related to date yes now I can use your apporach
-            but since I know the string should be long 23 - I think it is an acceplabele approach
-            but for the sake of the excercise - done :) 
-            */
-
-            //  OLD         expect(replaceResponce.shipDate.slice(0, 23)).to.be.equal(replacedOrder.shipDate.slice(0, 23));
             expect(helper.dateTrim(replaceResponce.shipDate)).to.be.equal(helper.dateTrim(replaceResponce.shipDate));
             expect(replaceResponce.status).to.be.equal(replacedOrder.status);
             expect(replaceResponce.complete).to.be.equal(replacedOrder.complete);
@@ -189,6 +180,9 @@ if you struggle with the method, dont waste much time on it, just calculate expe
     Scenario("Post order with shipDate format 'yyy-mm-dd' should have time set to T00:00:00.000+0000", function () {
 
         let order = orders.order03;
+        // @vicky - in this scenario i would prefer to see 'given' step containing just an order object, 
+        // 'when' step - an action, e.g. postOrder and 'then' step - all assertions (status=200, time assertion etc)
+
 
         Given("I have an existing order id", async function () {
 
@@ -197,6 +191,8 @@ if you struggle with the method, dont waste much time on it, just calculate expe
         });
 
         When("The return statis is 200", function () {
+
+            // @vicky - its better not to add messages to assertions. it just clutters things up, You have given-when-then description for that
 
             context.status.should.be.equal(200, 'Status for context');
 
@@ -218,6 +214,8 @@ if you struggle with the method, dont waste much time on it, just calculate expe
 
     Scenario("Calculate the current date", function () {
 
+        // @vicky - please see my comment above in the line 183-184
+
         let order = orders.order04;
 
         Given("I have an existing order id with unexistent day of month ", async function () {
@@ -231,8 +229,22 @@ if you struggle with the method, dont waste much time on it, just calculate expe
             context.status.should.be.equal(200, 'Status for context');
 
         });
+        //   @vicky - in cases when you don't know how to implement helper method or you just simply don't want to spend time on it you can simply 
+        //calculate the data manually and do the assertion, e.g.: 
 
-       
+        Then("The order date is calculated and saved correctly", function () {
+            let response = context.body;  
+            expect(response.shipDate).to.be.contain('2020-06-01');
+        }); 
 
+        When("user posts the order with inexistent month", async function () {
+            order.shipDate = '2020-13-01'
+            context = await client.postOrder(order);
+        });
+
+        Then("The order date is calculated and saved correctly", function () {
+            let response = context.body;  
+            expect(response.shipDate).to.be.contain('2021-01-01');
+        }); 
     });
 });
