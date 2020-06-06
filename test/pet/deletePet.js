96b0eea0e17eef9888f640fea80cc96a146d5283
angelica-serafini-cko <52659372+angelica-serafini-cko@users.noreply.github.com>
2020-06-06 16:51:15 +0100

    Viky's suggested implemented globally - I hope

--- test/pet/deletePet.js
+++ test/pet/deletePet.js
@@ -1,30 +1,31 @@
 const client = require('../client');
 const animal = require('../pets');
 
-feature('Pet with id can be deleted', function () {
+Feature('Pet with id can be deleted', function () {
 
-    scenario('the correct pet is deleted', function () {
+    Scenario('the correct pet is deleted', function () {
         let context;
-        let pet = animal.pet1;
-        // why do we have given and when staps named in a same way?
-        given("we delete a pet with id ", async function () {
+        let pet, id;
+        Given("we have a pet id to be deleted", async function () {
+            pet = animal.pet1;
             context = await client.postPet(pet);
             id = context.body.id
         });
-        when("we delete the pet by id", async function () {
+        When("we delete the pet by id", async function () {
             context = await client.deletePet(id);
         });
-        then("Status code is 200", function () {
+        Then("Status code is 200", function () {
             context.status.should.be.equal(200);
             context.statusCode.should.be.equal(200);
         });
-        // this sentence \'we call the deleted pet\' looks like 'when' clause, dont you think? this method should be  renamed to when() 
-        and("we call the deleted pet", async function () {
+        When("we call the deleted pet", async function () {
             context = await client.getPet(id);
         })
-        and("we verify the pet is no longer available, 404 Error not found", function () {
+        Then("we verify the pet is no longer available, with the correct code type and message", function () {
             context.statusCode.should.be.equal(404);
-            console.log(`\t --> Code: ${context.body.code} Type: ${context.body.type} message: ${context.body.message}`);
+            context.body.code.should.be.equal(1);
+            context.body.type.should.be.equal('error');
+            context.body.message.should.be.equal('Pet not found');
         })
-    }); // end of scenario
-});// end of feature
+    });
+});
--- test/pet/getPet.js
+++ test/pet/getPet.js
@@ -1,47 +1,50 @@
 const client = require('../client');
 const animal = require('../pets');
 
-feature('Verify the get functionality', function () {
-
-    scenario("Verify that correct pet is returned by id provided", function () {
-
+Feature('Verify the get functionality', function () {
+    Scenario("Verify that correct pet is returned by id provided", function () {
         let context;
-        let pet = animal.pet3;
-        given("we create a pet with unique id ", async function () {
+        let pet;
+        Given("we have a pet with unique id ", function () {
+            pet = animal.pet3;
+        });
+        When("we create a pet with unique id ", async function () {
             context = await client.postPet(pet);
             id = context.body.id
         });
-        when("we retrieve the pet by id", async function () {
+        And("we retrieve the pet by id", async function () {
             context = await client.getPet(id);
         });
-        then("Status code is 200", function () {
+        Then("Status code is 200", function () {
             context.status.should.be.equal(200)
         });
-        and("the correct ped id is returned ", function () {
+        And("the correct ped id is returned ", function () {
             context.body.id.should.be.equal(id);
         });
-        and("The correct pet name is returned", function () {
+        And("The correct pet name is returned", function () {
             context.body.name.should.be.equal(pet.name);
         });
-        and("The correct id and name category is returned", function () {
+        And("The correct id and name category is returned", function () {
             context.body.category.name.should.be.equal(pet.category.name);
             context.body.category.id.should.be.equal(pet.category.id);
         });
-        and("the correct info are in the photoUrls array", function () {
+        And("the correct info are in the photoUrls array", function () {
             context.body.photoUrls.should.be.an('array').that.does.include("http://t3est.com");
             context.body.photoUrls.should.be.an('array').that.does.include("http://t3est2.com");
         });
-        and("The correct pet status is returnd", function () {
+        And("The correct pet status is returnd", function () {
             context.body.status.should.be.equal(pet.status);
         })
     });
-
-    // can you make  the test case below in line with bdd approach please? (following given-when-then notation)
-
     scenario("Verify that pet is returned by specific large id", function () {
-        when("we retrieve the pet by id", async function () {
+        let context;
+        Given("We have a large id", function () {
             id = 423654262525634800n;
+        });
+        When("we retrieve the pet by id", async function () {
             context = await client.getPet(id);
+        });
+        Then("The pet is not found", async function () {
             context.body.message.should.be.equal("Pet not found")
         });
     });
